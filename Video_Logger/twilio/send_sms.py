bf75a053ba14b81b39f2f02d34106164fea97e25
rju <rju@purdue.edu>
2016-11-19 20:25:42 -0600

    Added template info for twilio

--- Video_Logger/twilio/send_sms.py
+++ Video_Logger/twilio/send_sms.py
@@ -1,1 +1,14 @@
 #twilio integration file
+
+# Download the twilio-python library from http://twilio.com/docs/libraries
+# pip install twilio should work, otherwise use easy_install twilio
+
+from twilio.rest import TwilioRestClient
+
+# Find these values at https://twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXX"
+auth_token = "YYYYYYYYYYYYYYYYYY"
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.messages.create(to="+12316851234", from_="+15555555555",
+                                     body="Hello there!")
